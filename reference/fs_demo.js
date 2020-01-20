42610a1658a42c1804c814755ffad1a6761305dc
CO16348Sahil <35717032+CO16348Sahil@users.noreply.github.com>
2020-01-20 21:22:29 +0530

    After file system

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -9,7 +9,25 @@ const path = require('path')
 // });
 
 // Create and write to file
-fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),'Hello world how are you?',(err)=>{ 
-    if(err) throw err;
-    console.log('File written to..');
-});
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'),'Hello world how are you?',(err)=>{ 
+//     if(err) throw err;
+//     console.log('File written to..');
+// });
+
+//Append in a file
+// fs.appendFile(path.join(__dirname, '/test', 'hello.txt'),'And this is appended text',(err)=>{ 
+//     if(err) throw err;
+//     console.log('File appended..');
+// });
+
+//Read file
+// fs.readFile(path.join(__dirname, '/test', 'hello.txt'),'utf8',(err, data)=>{ 
+//     if(err) throw err;
+//     console.log(data);
+// });
+
+//Rename a file
+// fs.rename(path.join(__dirname, '/test', 'hello.txt'),path.join(__dirname, '/test', 'hello_renamed.txt'),(err)=>{ 
+//     if(err) throw err;
+//     console.log('File written to..');
+// });
